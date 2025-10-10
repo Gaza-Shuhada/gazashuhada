c71669e26cce8db80c58c43a6795db6b7ee5e86d
Jens Munch <jens.a.munch@gmail.com>
2025-10-10 20:46:26 +0200

    hotfix for name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Gaza Martyrs شهداء غزة",
+  title: "Gaza Witnesses شهداء غزة",
   description: "",
   icons: {
     icon: [
